18c8e9b0132d321ae5e573f970b7f63657dd2492
Emotions <estskywalker@gmail.com>
2023-02-26 23:57:43 +0700

    add scene tool

---
+++ Assignment2/Scenes.java
@@ -0,0 +1,45 @@
+public class Scenes {
+
+    private static String[][][][] scene1 = {
+        {null, null, null, null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null, null, null, null},
+        {BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode},
+        {BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode},
+        {BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode},
+        {BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode},
+        {BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode}
+    };
+
+    private static String[][][][] scene3 = {
+        {null, null, null, null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null, null, null, null},
+        {BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode, BlockCodeColor.grassColorCode},
+        {BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode},
+        {BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode},
+        {BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode},
+        {BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode},
+        {BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode, BlockCodeColor.dirtColorCode}
+    };
+
+    private static String[][][][] end = {
+        {null, null, null, null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null, null, null, null}
+    };
+
+    public static String[][][][][] scenes = {
+        scene1, scene3, scene1, scene3, end
+    };
+}
